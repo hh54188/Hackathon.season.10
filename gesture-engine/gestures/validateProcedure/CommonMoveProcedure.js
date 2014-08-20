cd776d71300f2d69c31ef729704037461216c1f4
unknown <liguangyi@A2062EB22851B99.internal.baidu.com>
2014-08-20 19:27:24 +0800

    update

--- gesture-engine/gestures/validateProcedure/CommonMoveProcedure.js
+++ gesture-engine/gestures/validateProcedure/CommonMoveProcedure.js
@@ -2,22 +2,52 @@ define(["base"], function (BaseProcedure) {
 
     function CommonMoveProcedure (gesture, nextProcedure) {
 
+        this.resetFrameCount();
+
         this.gesture = gesture;
         this.successor = nextProcedure || null;
     }
 
     CommonMoveProcedure.prototype = Object.create(BaseProcedure, {
 
+        // 注意，这个规则是否正确？
+        resetFrameCount: {
+            value: function (frame) {
+
+                this.frameCount = this.maxFrameNum;
+            }
+        },
+
         validate: {
             value: function (frame) {
 
+                this.frameCount--;
                 var gesture = this.gesture;
-                if (gesture.validateGestureOnMove(frame) 
-                        && gesture.validateGestureBasicCondition(frame)) {
-                    return true;    
+
+                // 手势验证不通过
+                if (!gesture.validateGestureOnMove(frame) 
+                        || !gesture.validateGestureBasicCondition(frame)) {
+
+                    this.resetFrameCount();
+                    return false;
                 }
 
-                return false;
+                // When to triger the next procedure ?
+                if (!this.frameCount) {
+                    var nextResult = this.validateNext(frame);
+                    if (nextResult) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    var selfResult = this.validateSelf(frame);
+                    if (selfResult) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
             }
         }
     });
--- gesture-engine/gestures/validateProcedure/CommonStartProcedure.js
+++ gesture-engine/gestures/validateProcedure/CommonStartProcedure.js
@@ -14,19 +14,45 @@ define(["base"], function (BaseProcedure) {
 
         validate: {
             value: function (frame) {
+                
+                this.frameCount--;
 
                 // 如果是第一次进入验证
                 if (this.frameCount == this.maxFrameNum) {
                     if (this.gesture.validateGestureStart(frame)) {
 
                         // 如果验证通过并且只有一帧，手势识别成功
-                        if (!--this.frameCount) {
+                        if (!this.frameCount) {
                             return true;
                         }
 
                         return false;
                     }
                 }
+
+                var nextResult = this.validateNext(frame);
+
+                // 如果下一个环节执行结果为true，并且倒计时完毕
+                // 则手势验证完毕
+                if (nextResult && !this.frameCount) {
+                    return true;
+
+                // 如果下一个执行结果为true，则继续倒计时
+                } else if (nextResult) {
+                    return false;
+
+                // 如果下一个环节执行返回为false，则需要重新开始
+                } else if (!nextResult) {
+
+                    this.frameCount = this.maxFrameNum;
+                    return false;
+                }
+
+
+
+
+
+
 
                 //如果next返回true，则继续倒计时
                 //如果返回false，重置计时器
