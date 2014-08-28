edf501859a5a246e623fc14298973181a2f5a2a6
unknown <liguangyi@A2062EB22851B99.internal.baidu.com>
2014-08-28 17:41:36 +0800

    updae

--- test/baidu_product/image.js
+++ test/baidu_product/image.js
@@ -26,20 +26,55 @@ var dock = $(".album-pnl"); // 图片列表
 
 var rotateX = 0,
 	rotateY = 0,
-	rotateZ = 0;
+	rotateZ = 0,
+	translateX = 0,
+	translateY = 0,
+	translateZ = 0;
 
 function generateTransform () {
-	return "rotateX(" + rotateX + "deg) rotateY(" + rotateY + "deg) rotateZ(" + rotateZ + "deg)";
+
+	return "translateX(" + translateX + "px) 
+			translateY(" + translateY + "px) 
+			translateZ(" + translateZ + "px) 
+			rotateX(" + rotateX + "deg) 
+			rotateY(" + rotateY + "deg) 
+			rotateZ(" + rotateZ + "deg)";
 }
 
 window.API = {
 
+	threed: {
+
+		zoomIn: function () {
+			translateZ++;
+			img.style.transform = generateTransform();
+		},
+
+		zoomOut: function () {
+			translateZ--;
+			img.style.transform = generateTransform();
+		},
+
+		translateOnSurface: function (xAct, yAct) {
+
+			translateX = xAct? ++translateX: --translateX;
+			translateY = yAct? ++translateY: --translateY;
+
+			img.style.transform = generateTransform();
+		}
+
+		rotateOn3d: function () {
+			// TODO	
+		}
+	},
+
+	/**
+	 * 初始化页面, 给容器添加3d属性
+	 */
 	init: function () {
 		var body = document.body;
 		body.style.perspective = "1000px";
-
 		img.style.transformStyle = "preserve-3d";
-		// img.style.transform
 	},
 
 	/**
